16bdbb4b60f0e38a57adc2e7bd06b2b35ff76f4f
root <root@majorbaker.tk>
2016-10-24 15:45:11 -0500

    fix print statements

--- Python_Bot.py
+++ Python_Bot.py
@@ -21,14 +21,14 @@ async def on_message(message):
 
     #recall 5 most recent terms
     if message.content == '!r' :
-        commands.recent()
+        commands.termSearch(commands.recent())
 
     #link to t&c
     if message.content == '!link' :
         await client.send_message(message.channel, 'http://bit.do/termcon')
 
     if message.content == '!help' :
-        commands.help()
+        commands.termSearch(commands.help())
 
 
 client.run('buymyasianbaby@gmail.com','suchpassword1')
