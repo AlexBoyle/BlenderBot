22a0d2992716d7ecf9130d19e5efe941651c2250
root <root@majorbaker.tk>
2016-10-27 11:21:31 -0500

    added vote

--- Python_Bot.py
+++ Python_Bot.py
@@ -1,51 +1,67 @@
 #!/usr/bin/python3
- 
+
 import discord
 import asyncio
 import random
- 
+
 import TermCommands
 import PingCommands
 import VoteCommands
- 
 
- 
- 
+
+
+
 def terms(message):
      #reference term by number
     if message.content.startswith('!t '):
         return TermCommands.term(message.content[3:])
- 
+
     #search through terms
     if message.content.startswith('!ts '):
         return TermCommands.termSearch(message.content[4:])
- 
+
     #recall 5 most recent terms
     if message.content == '!r' :
         return  TermCommands.recent()
- 
+
     #link to t&c
     if message.content == '!link' :
         return 'http://bit.do/termcon'
- 
+
     #source code
     if message.content == '!code' :
         return 'https://github.com/AlexBoyle/Spicier_Bot'
- 
+
     #pull up list of TermCommands
     if message.content == '!help' :
         return TermCommands.help()
- 
+
     #Finds and prints all terms referenced in the last !ts or !t command
     if message.content == '!ref':
         return TermCommands.reference()
- 
+
     #Random term
     if message.content == '!tr':
          return TermCommands.term(str(random.randint(1,len(TermCommands.termlist))))
- 
-#def vote(message):
 
+def vote(message):
+    if message.content.startswith("%vote "):
+        return VoteCommands.callVote(message)
+
+    if message.content.startswith("%y"):
+        return VoteCommands.yesVote(message)
+
+    if message.content.startswith("%n"):
+        return VoteCommands.noVote(message)
+
+    if message.content.startswith("%results"):
+        return VoteCommands.results()
+
+    if message.content.startswith("%clear"):
+        return VoteCommands.clear()
+
+    if message.content.startswith("%help"):
+        return VoteCommands.help()
 
 def ping(message):
     if message.content.strip() == ">ly":
@@ -60,24 +76,23 @@ client = discord.Client()
  
 @client.event
 async def on_message(message):
-    print(message.content)
 
     #Terms and conditions commands
     if message.content.startswith('!'):
         out = terms(message)
         if out != None:
             await client.send_message(message.channel,out)
- 
+
     #Ping Commands
     if message.content.startswith('>'):
         out = ping(message)
         if out != None:
             await client.send_message(message.channel,out)
- 
+
     #Vote Commands
     if message.content.startswith('%'):
         out = vote(message)
         if out != None:
             await client.send_message(message.channel,out)
- 
+
 client.run('buymyasianbaby@gmail.com','suchpassword1')
--- VoteCommands.py
+++ VoteCommands.py
@@ -1,1 +1,59 @@
-print("nothing here")
+currentVote = ""
+yes = []
+no = []
+commandlist = [['%vote [topic]', 'set a vote for topic'],['%y[es]', 'submit a yes vote'],
+    ['%n[0]', 'submit a no vote'],['%results', 'get results for current vote'],['%clear', 'clear all data for the current vote']]
+
+
+def callVote(message):
+    global currentVote
+    global yes
+    global no
+    if currentVote == "":
+        currentVote = message.content[6:]
+        yes = []
+        no = []
+        return "Vote for '%s' started please vote yes  or no " % currentVote
+    return "there is already a vote"
+
+def yesVote(message):
+    global yes
+    global no
+    id = message.author.id
+    if id in no:
+        no.remove(id)
+    if id not in yes:
+        yes.append(id)
+    return  None
+def noVote(message):
+    global yes
+    global no
+    id = message.author.id
+    if id in yes:
+        yes.remove(id)
+    if id not in no:
+        no.append(id)
+    return None
+def results():
+    if currentVote == "":
+        return "There is no vote right now"
+    out = "%s \nYes votes(%i):\n" % (currentVote,len(yes))
+    for e in yes:
+        out += "<@" + e + ">\n"
+    out += "No votes(%i):\n" % len(no)
+    for e in no:
+        out += "<" + e + ">\n"
+    return out
+def clear():
+    global currentVote
+    global yes
+    global no
+    currentVote = ""
+    yes = []
+    no = []
+def help():
+    output = ""
+    for entry in commandlist:
+          output += ('Use "%s" to %s! \n' % (entry[0],entry[1]))
+    return output
+
